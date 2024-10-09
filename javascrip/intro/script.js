9ae0dbf3954d0e8efa66f70856dee4ccc7322743
faniv <fanivec@gmail.com>
2024-10-09 18:41:14 -0300

    Clases de js

---
+++ javascrip/intro/script.js
@@ -0,0 +1,5 @@
+/* Introducción*/ 
+//Comentario de línea
+alert("Hola mundo desde script.js");
+document.write("<h1>Hola mundo desde js con document.write</h1><hr>");
+console.log("Hola esto es un mensaje en consola desde el script");
